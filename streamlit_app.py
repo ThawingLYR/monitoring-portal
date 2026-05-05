8c78444acdb863c02092651ff043b87547cb5aaa
Louis Pauchet <louis.pauchet@insa-rouen.fr>
2026-05-05 15:35:30 +0200

    Update streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-home_page = st.Page("about.py", title="About", icon=":material/home:")
+home_page = st.Page("src/app/pages/about.py", title="About", icon=":material/home:")
 
 ground_temperature = st.Page(
     "src/app/pages/main_boreholes_temperature.py",
@@ -9,39 +9,53 @@ ground_temperature = st.Page(
     icon=":material/thermostat:",
 )
 ground_water_content = st.Page(
-    "borehole_ground_water_content.py",
+    "src/app/pages/borehole_ground_water_content.py",
     title="Ground water content",
     icon=":material/water_drop:",
 )
 weather_stations = st.Page(
-    "main_weather_stations.py", title="Weather stations", icon=":material/cloud:"
+    "src/app/pages/main_weather_stations.py",
+    title="Weather stations",
+    icon=":material/cloud:",
 )
 insar_deformation = st.Page(
-    "insar_deformation.py", title="InSAR deformation", icon=":material/satellite_alt:"
+    "src/app/pages/insar_deformation.py",
+    title="InSAR deformation",
+    icon=":material/satellite_alt:",
 )
 all_sky_camera = st.Page(
-    "all_sky_camera.py", title="All-sky camera", icon=":material/360:"
+    "src/app/pages/all_sky_camera.py", title="All-sky camera", icon=":material/360:"
 )
 time_lapse_cameras = st.Page(
-    "time_lapse_cameras.py", title="Time-lapse cameras", icon=":material/photo_camera:"
+    "src/app/pages/time_lapse_cameras.py",
+    title="Time-lapse cameras",
+    icon=":material/photo_camera:",
 )
 
 ground_ice_content = st.Page(
-    "ground_ice_content.py", title="Ground ice content", icon=":material/mode_cool:"
+    "src/app/pages/ground_ice_content.py",
+    title="Ground ice content",
+    icon=":material/mode_cool:",
 )
 geomorphology = st.Page(
-    "geomorphology.py", title="Geomorphology", icon=":material/landscape:"
+    "src/app/pages/geomorphology.py", title="Geomorphology", icon=":material/landscape:"
 )
 
 landslide_model = st.Page(
-    "landslide_model.py", title="Landslide model", icon=":material/landslide:"
+    "src/app/pages/landslide_model.py",
+    title="Landslide model",
+    icon=":material/landslide:",
 )
 weather_model = st.Page(
-    "weather_model.py", title="Weather model (high resolution)", icon=":material/rainy:"
+    "src/app/pages/weather_model.py",
+    title="Weather model (high resolution)",
+    icon=":material/rainy:",
 )
 
 instrument_status = st.Page(
-    "instrument_status.py", title="Boreholes status", icon=":material/battery_alert:"
+    "src/app/pages/instrument_status.py",
+    title="Boreholes status",
+    icon=":material/battery_alert:",
 )
 
 pg = st.navigation(
