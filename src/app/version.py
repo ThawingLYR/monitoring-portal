aaaf2f10d66ffc78fbeb98ecd8266e097cb69d0f
Louis Pauchet <louis.pauchet@insa-rouen.fr>
2026-05-05 18:20:22 +0200

    Add version to about page

--- src/app/version.py
+++ src/app/version.py
@@ -1,2 +1,2 @@
 # This file contains the version number of the application for use by the CI/CD pipeline and for display in the app. The version number is automatically updated by the CI/CD pipeline when a new release is created on GitHub, following semantic versioning principles (https://semver.org/).
-VERSION = "0.0.0"
+VERSION = "0.0.0-Development version (version number will be updated upon release)"
