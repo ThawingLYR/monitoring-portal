eb15c2ce10dd2b84e2ab24d1b9a0bb383ab59a6d
Louis Pauchet <louis.pauchet@insa-rouen.fr>
2026-05-12 21:17:24 +0200

    fix linting

--- src/datasource/__init__.py
+++ src/datasource/__init__.py
@@ -1,3 +1,5 @@
 from src.datasource.datasource_model import DataSource
-from src.datasource.frost import FrostDataSource
-from src.datasource.tilsig import TilsigDataSource
+from src.datasource.frost import FrostDataSource  # noqa: F401
+from src.datasource.tilsig import TilsigDataSource  # noqa: F401
+
+__all__ = ["DataSource"]
