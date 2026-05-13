841998bcdd39d2ea0184bb296fdd600a2cd2e90e
Louis Pauchet <louis.pauchet@insa-rouen.fr>
2026-05-13 11:43:42 +0200

    Update example caching job

--- cache-cache-test.py
+++ cache-cache-test.py
@@ -1,5 +1,6 @@
 from src.config.config_manager import ConfigManager
 from src.sensors.borehole import SensorBorehole
+from src.plots.test_fig import PlotTimeseriesBoreholes
 
 from dotenv import load_dotenv
 
@@ -8,7 +9,8 @@ load_dotenv()
 
 config_manager = ConfigManager()
 config_manager.load_config("boreholes")
-configs = config_manager.get_stations("boreholes", query={"dataProvider": "tilsig"})
+configs = config_manager.get_stations("boreholes")
 for config in configs:
     sensor = SensorBorehole(config=config)
     sensor.update_latest_data()
+    sensor.prepare_figure(PlotTimeseriesBoreholes)
