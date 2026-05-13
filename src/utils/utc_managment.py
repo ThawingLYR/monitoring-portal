22c0fc2022086606ad27539cad152ce92b69bc58
Louis Pauchet <louis.pauchet@insa-rouen.fr>
2026-05-13 11:01:36 +0200

    bug fix

--- src/utils/utc_managment.py
+++ src/utils/utc_managment.py
@@ -5,6 +5,9 @@ def make_utc(time: datetime):
     if time is None:
         return time
 
+    if isinstance(time, str):
+        time = datetime.fromisoformat(time)
+
     if time.tzinfo is None:
         time = time.replace(tzinfo=timezone.utc)
     return time
