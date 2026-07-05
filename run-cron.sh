7c185d8235743b54e830961248a536881dff55c2
Louis Pauchet <louis.pauchet@insa-rouen.fr>
2026-07-05 21:12:32 +0200

    Disable the repo cloning

--- run-cron.sh
+++ run-cron.sh
@@ -13,19 +13,19 @@ INITIAL_DIR=$(pwd)
 
 
 
-echo "Making sure we are using the latest configuration..."
-if [ -d "$CONFIG_FOLDER" ]; then
-    # If the folder exists, pull the latest changes
-    echo "Folder '$CONFIG_FOLDER' exists. Pulling the latest changes..."
-    cd "$CONFIG_FOLDER" || exit
-    git pull origin main
-    # Return to the initial directory
-    cd "$INITIAL_DIR" || exit
-else
-    # If the folder does not exist, clone the repository
-    echo "Folder '$CONFIG_FOLDER' does not exist. Cloning the repository..."
-    git clone "$REPO_URL" "$CONFIG_FOLDER"
-fi
+# echo "Making sure we are using the latest configuration..."
+# if [ -d "$CONFIG_FOLDER" ]; then
+#     # If the folder exists, pull the latest changes
+#     echo "Folder '$CONFIG_FOLDER' exists. Pulling the latest changes..."
+#     cd "$CONFIG_FOLDER" || exit
+#     git pull origin main
+#     # Return to the initial directory
+#     cd "$INITIAL_DIR" || exit
+# else
+#     # If the folder does not exist, clone the repository
+#     echo "Folder '$CONFIG_FOLDER' does not exist. Cloning the repository..."
+#     git clone "$REPO_URL" "$CONFIG_FOLDER"
+# fi
 
 echo "Let's run the cron jobs..."
 
